83991bc76de37e9c68fb3c5f21ad91f078a72926
Peter Jones <pjones@redhat.com>
2019-10-15 10:18:27 -0400

    Make die_if_checked_out() prune missing checkouts of unlocked worktrees.
    Currently if you do, for example:
    
    $ git worktree add path foo
    
    And "foo" has already been checked out at some other path, but the user
    has removed it without pruning, and the worktree is not locked, you'll
    get an error that the branch is already checked out.  It isn't
    meaningfully checked out, the repo's data is just stale and no longer
    reflects reality.
    
    This makes it so that if nothing is present where a worktree is
    supposedly checked out, and it is not locked, we ignore that the
    worktree exists, then it should be cleaned up.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- branch.c
+++ branch.c
@@ -360,6 +360,12 @@ void die_if_checked_out(const char *branch, int ignore_current_worktree)
 	wt = find_shared_symref("HEAD", branch);
 	if (!wt || (ignore_current_worktree && wt->is_current))
 		return;
+
+	if (prune_worktree_if_missing(wt) >= 0) {
+		delete_worktrees_dir_if_empty();
+		return;
+	}
+
 	skip_prefix(branch, "refs/heads/", &branch);
 	die(_("'%s' is already checked out at '%s'"),
 	    branch, wt->path);
